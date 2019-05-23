c41b6e058e1d3395751ca1a90c76cf084d3cffd1
liuxunjie <1146595231@qq.com>
2019-05-23 11:04:17 +0800

    Merge branch '删除' of https://github.com/thefourthgroupjie/medicine
    # Conflicts:
    #	.idea/compiler.xml
    #	.idea/encodings.xml
    #	.idea/misc.xml
    #	pom.xml

--- cluod-feign-cls/src/main/java/com/xb/service/XbServiceFeign.java
+++
@@ -1,7 +0,0 @@
-package com.xb.service;
-
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient(value = "provide-service")
-public interface XbServiceFeign extends XbServiceApi {
-}
